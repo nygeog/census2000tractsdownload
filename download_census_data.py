ff54c07e0955971bd093fb86384dfa0d11a78e84
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2013-05-22 19:31:56 -0400

    removed personal dir
    removed personal dir

--- download_census_data.py
+++ download_census_data.py
@@ -5,7 +5,8 @@ url_2nd = "_d00_shp.zip"
 state_fips = ["01","02","04","05","06","08","09","10","11","12","13","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","44","45","46","47","48","49","50","51","53","54","55","56","72"]
 stop_time = 3
 
-download_location = "/Users/danielmsheehan/Downloads/census_2000"
+#put your file location here between quotes ie. "/Users/<YOURUSERNAME>/Downloads/census_2000"
+download_location = "/Users/<YOURUSERNAME>/Downloads/census_2000"
 
 #loop through all the state fips
 for stfi in state_fips:
